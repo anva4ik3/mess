f715a0e045f19bb8509cb19db044c183fc23d15c
anva4ik3 <replit3333333333replit@gmail.com>
2026-04-14 17:58:00 +0500

    Update MainActivity.kt

--- android/app/src/main/java/com/yourname/messenger/MainActivity.kt
+++ android/app/src/main/java/com/yourname/messenger/MainActivity.kt
@@ -7,17 +7,19 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.ui.Modifier
+import com.yourname.messenger.ui.ChatScreen
 import com.yourname.messenger.ui.theme.MessengerTheme
-import com.yourname.messenger.viewmodel.ChatViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MessengerTheme {
-                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    // TODO: Подключить навигацию и DI после базовой сборки
-                    ChatScreen(viewModel = ChatViewModel())
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+                    ChatScreen()
                 }
             }
         }
